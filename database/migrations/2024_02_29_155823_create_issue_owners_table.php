66b0ce895d88c14150da2c68134439322fc898b4
Mohammed Ragab El Ashmoney <m.ragab.elashmoney@outlook.com>
2024-03-03 12:38:47 +0200

    Modifying issue owners table

--- database/migrations/2024_02_29_155823_create_issue_owners_table.php
+++ database/migrations/2024_02_29_155823_create_issue_owners_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('issue_owners', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('owner_id');
+            $table->string('owner_name');
             $table->timestamps();
         });
     }
