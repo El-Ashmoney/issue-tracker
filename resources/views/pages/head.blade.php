950f294f0762f5a41d275d859dd891405bef3759
Mohammed Ragab El Ashmoney <m.ragab.elashmoney@outlook.com>
2024-03-10 13:54:40 +0200

    Seperate the head tag and fix the css location

---
+++ resources/views/pages/head.blade.php
@@ -0,0 +1,34 @@
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+<title>Issue Tracker</title>
+<meta name="description" content="Excelent Protection Issue Tracker" />
+
+<!-- Favicon -->
+<link rel="icon" type="image/x-icon" href="{{ asset('img/favicon/favicon.ico') }}" />
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap" rel="stylesheet" />
+
+<link rel="stylesheet" href="{{ asset('vendor/fonts/materialdesignicons.css') }}" />
+
+<!-- Menu waves for no-customizer fix -->
+<link rel="stylesheet" href="{{ asset('vendor/libs/node-waves/node-waves.css') }}" />
+
+<!-- Core CSS -->
+<link rel="stylesheet" href="{{ asset('vendor/css/core.css') }}" class="template-customizer-core-css" />
+<link rel="stylesheet" href="{{ asset('vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
+<link rel="stylesheet" href="{{ asset('css/demo.css') }}" type="text/css" />
+
+<!-- Vendors CSS -->
+<link rel="stylesheet" href="{{ asset('vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+<link rel="stylesheet" href="{{ asset('vendor/libs/apex-charts/apex-charts.css') }}" />
+
+<!-- Page CSS -->
+
+<!-- Helpers -->
+<script src="{{ asset('vendor/js/helpers.js') }}"></script>
+<!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+<!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+<script src="{{ asset('js/config.js') }}"></script>
