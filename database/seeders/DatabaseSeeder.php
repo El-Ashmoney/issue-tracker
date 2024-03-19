062be9835085cfc1be70fb61037a52a0f53cb6e0
Mohammed Ragab El Ashmoney <m.ragab.elashmoney@outlook.com>
2024-03-19 10:25:13 +0200

    add all the data via seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\IssueAssigneesSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        // $this->call(SectorsSeeder::class);
+        // $this->call(IssueAssigneesSeeder::class);
     }
 }
