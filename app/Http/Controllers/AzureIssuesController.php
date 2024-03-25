fadd101ec6e9f63e134571b0389e18db09de1428
Mohammed Ragab El Ashmoney <m.ragab.elashmoney@outlook.com>
2024-03-25 12:29:45 +0200

    Add Azure model, controller and migration

---
+++ app/Http/Controllers/AzureIssuesController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AzureIssuesController extends Controller
+{
+    //
+}
---
+++ database/migrations/2024_03_25_095205_create_azure_issues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('azure_issues', function (Blueprint $table) {
+            $table->unsignedBigInteger('work_item_id')->primary();
+            $table->string('title');
+            $table->text('description');
+            $table->string('project');
+            $table->string('issue_assignee');
+            $table->string('status');
+            $table->string('priority');
+            $table->string('discipline');
+            $table->string('teams');
+            $table->string('source');
+            $table->string('WorkedTime');
+            $table->string('description_of_close');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('azure_issues');
+    }
+};
